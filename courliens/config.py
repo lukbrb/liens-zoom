e09a2b395bce31caf93dd5edcf9e185e823b300b
Lucas <lucasbarbr@outlook.com>
2023-08-23 21:46:54 +0200

    adaptation des chemins aux nouveaux dossiers

---
+++ courliens/config.py
@@ -0,0 +1,8 @@
+""" Relative paths for pictures, icons and database"""
+
+WINDOW_ICON = "imgs/zoom_app.ico"
+START_ICON = "imgs/start_pic.jpg"
+
+MODIF_UI = "ui/modif.ui"
+
+DB_PATH = "data/infozoom.db"
