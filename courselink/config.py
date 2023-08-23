49e6845e5183fba7002c489df672f57e41c7dc2d
Lucas <lucasbarbr@outlook.com>
2023-08-23 21:46:04 +0200

    renomme courselink en courliens

--- courselink/config.py
+++
@@ -1,8 +0,0 @@
-""" Relative paths for pictures, icons and database"""
-
-WINDOW_ICON = "resources/zoom_app.ico"
-START_ICON = "resources/start_pic.jpg"
-
-MODIF_UI = "resources/modif.ui"
-
-DB_PATH = "data/infozoom.db"
